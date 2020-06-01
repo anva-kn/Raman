e60066d545e7bfaefbdf295d9c1724281af4a49e
sterini <44762464+sterini@users.noreply.github.com>
2020-06-01 09:24:29 +0800

    analye 7

--- ste_model_spectrum.py
+++ ste_model_spectrum.py
@@ -131,6 +131,15 @@ def positive_mse_loss(beta, X, Y):
     error = sum([positive_mse(p(X[i]), Y[i]) for i in range(X.size)])/X.size
     return(error)
 
+def reg_positive_mse(y_pred,y_true):                
+    loss_pos=(y_pred-y_true)**2
+    loss_neg=np.dot((y_pred-y_true<0),(y_pred-y_true)**8)
+    loss=loss_pos+loss_neg
+    return np.sum(loss)/y_true.size
+
+def reg_pos_loss(beta, X, Y,function):                
+    return positive_mse(function(X,beta), Y)
+
 
 def mse_loss(beta,X, Y,function):
     return sum((function(X,beta)-Y)**2)/Y.size
@@ -293,7 +302,6 @@ def poly4(x_data,beta):
         p=np.poly1d(beta)
         return p(x_data)
         
-
 def positive_mse(y_pred,y_true):                
     loss=np.dot((10**8*(y_pred-y_true>0)+np.ones(y_true.size)).T,(y_pred-y_true)**2)
     return np.sum(loss)/y_true.size
@@ -301,6 +309,14 @@ def positive_mse(y_pred,y_true):
 def pos_mse_loss(beta, X, Y,function):                
     return positive_mse(function(X,beta), Y)
 
+def reg_positive_mse(y_pred,y_true):                
+    loss_pos=(y_pred-y_true)**2
+    loss_neg=np.dot((y_pred-y_true>0),np.abs((y_pred-y_true)))
+    loss=loss_pos+loss_neg
+    return np.sum(loss)/y_true.size
+
+def reg_pos_mse_loss(beta, X, Y,function):                
+    return reg_positive_mse(function(X,beta), Y)
 
 def remove_est_florescence(f_sup,data_sub):
     
@@ -316,7 +332,9 @@ def remove_est_florescence(f_sup,data_sub):
     beta_init=np.polyfit(f_sup,min_data_amm,4)
     beta_init[4]=beta_init[4]+min(min_data_amm-poly_min)
     
-    result = minimize(pos_mse_loss, beta_init, args=(f_sup,min_data_amm,poly4), method='Nelder-Mead', tol=1e-12)
+    #result = minimize(pos_mse_loss, beta_init, args=(f_sup,min_data_amm,poly4), method='Nelder-Mead', tol=1e-12)
+    #result = minimize(mse_loss, beta_init, args=(f_sup,min_data_amm,poly4), method='Nelder-Mead', tol=1e-12)
+    result = minimize(reg_pos_mse_loss, beta_init, args=(f_sup,min_data_amm,poly4), method='Nelder-Mead', tol=1e-12)
     
     beta_hat = result.x                 
         
