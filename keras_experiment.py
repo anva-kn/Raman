1b349596604dcf9ee3b234447f73720cc13ae30f
Anvar Kunanbayev <kunanbaevanvar@gmail.com>
2020-08-16 22:56:12 +0800

    fixed loading dimensions

--- keras_experiment.py
+++ keras_experiment.py
@@ -31,31 +31,47 @@ mix_150_area_4 = pd.read_csv("data/MG-5_150_ppb/area4_Exported.dat", sep="\s+", 
 ################################ PREPROCESS THE DATA ###########################################
 
 #-------------------------------    15000 PPB    ----------------------------------------------
+length = mix_15000_area_1.shape[1] - 1
 f_sup = np.array(mix_15000_area_1.iloc[:, 0], dtype=np.float64)
-data_15000_area1 = np.array(mix_15000_area_1.iloc[:, 1:], dtype=np.float64)
-data_15000_area2 = np.array(mix_15000_area_2.iloc[:, 1:], dtype=np.float64)
-data_15000_area3 = np.array(mix_15000_area_3.iloc[:, 1:], dtype=np.float64)
-data_15000_area4 = np.array(mix_15000_area_4.iloc[:, 1:], dtype=np.float64)
+data_15000_area1 = np.zeros((length, f_sup.shape[0]))
+data_15000_area2 = np.zeros((length, f_sup.shape[0]))
+data_15000_area3 = np.zeros((length, f_sup.shape[0]))
+data_15000_area4 = np.zeros((length, f_sup.shape[0]))
 
-#-------------------------------    1500 PPB    -----------------------------------------------
-data_1500_area1 = np.array(mix_1500_area_1.iloc[:, 1:], dtype=np.float64)
-data_1500_area2 = np.array(mix_1500_area_2.iloc[:, 1:], dtype=np.float64)
-data_1500_area3 = np.array(mix_1500_area_3.iloc[:, 1:], dtype=np.float64)
-data_1500_area4 = np.array(mix_1500_area_4.iloc[:, 1:], dtype=np.float64)
+data_1500_area1 = np.zeros((length, f_sup.shape[0]))
+data_1500_area2 = np.zeros((length, f_sup.shape[0]))
+data_1500_area3 = np.zeros((length, f_sup.shape[0]))
+data_1500_area4 = np.zeros((length, f_sup.shape[0]))
 
-#-------------------------------    150 PPB    ------------------------------------------------
-data_150_area1 = np.array(mix_150_area_1.iloc[:, 1:], dtype=np.float64)
-data_150_area2 = np.array(mix_150_area_2.iloc[:, 1:], dtype=np.float64)
-data_150_area3 = np.array(mix_150_area_3.iloc[:, 1:], dtype=np.float64)
-data_150_area4 = np.array(mix_150_area_4.iloc[:, 1:], dtype=np.float64)
+data_150_area1 = np.zeros((length, f_sup.shape[0]))
 
+j = 0
+for i in range(1, length):
 
-################################ SAVE THE DATA ###########################################
+    data_15000_area1[j] = np.array(mix_15000_area_1.iloc[:, i], dtype=np.float64)
+    data_15000_area2[j] = np.array(mix_15000_area_2.iloc[:, i], dtype=np.float64)
+    data_15000_area3[j] = np.array(mix_15000_area_3.iloc[:, i], dtype=np.float64)
+    data_15000_area4[j] = np.array(mix_15000_area_4.iloc[:, i], dtype=np.float64)
+
+# #-------------------------------    1500 PPB    -----------------------------------------------
+    data_1500_area1[j] = np.array(mix_1500_area_1.iloc[:, i], dtype=np.float64)
+    data_1500_area2[j] = np.array(mix_1500_area_2.iloc[:, i], dtype=np.float64)
+    data_1500_area3[j] = np.array(mix_1500_area_3.iloc[:, i], dtype=np.float64)
+    data_1500_area4[j] = np.array(mix_1500_area_4.iloc[:, i], dtype=np.float64)
+# #-------------------------------    150 PPB    ------------------------------------------------
+    data_150_area1[j] = np.array(mix_150_area_1.iloc[:, i], dtype=np.float64)
+    # data_150_area2 = np.array(mix_150_area_2.iloc[:, 1:], dtype=np.float64)
+    # data_150_area3 = np.array(mix_150_area_3.iloc[:, 1:], dtype=np.float64)
+    # data_150_area4 = np.array(mix_150_area_4.iloc[:, 1:], dtype=np.float64)
+    j += 1
+#
+#
+# ################################ SAVE THE DATA ###########################################
 save_to_file=1
 
-data_15000 = np.zeros([4, f_sup.shape[0], data_15000_area1.shape[1]])
-data_1500 = np.zeros([4, f_sup.shape[0], data_15000_area1.shape[1]])
-data_150 = np.zeros([4, f_sup.shape[0], data_15000_area1.shape[1]])
+data_15000 = np.zeros([4, length, f_sup.shape[0]])
+data_1500 = np.zeros([4, length, f_sup.shape[0]])
+data_150 = np.zeros([4, length, f_sup.shape[0]])
 
 data_15000[0], data_15000[1], data_15000[2], data_15000[3] = data_15000_area1, data_15000_area2, data_15000_area3, data_15000_area4
 data_1500[0], data_1500[1], data_1500[2], data_1500[3] = data_1500_area1, data_1500_area2, data_1500_area3, data_1500_area4
@@ -64,12 +80,12 @@ data_1500[0], data_1500[1], data_1500[2], data_1500[3] = data_1500_area1, data_1
 
 if save_to_file:
     filename = 'shelve_save_data.out'
-    
+
     my_shelf = shelve.open(filename)
-    
+
     my_shelf["data_15000"] = data_15000
     my_shelf["data_1500"] = data_1500
     my_shelf["data_150"] = data_150
     my_shelf["f_sup"] = f_sup
-    
+
     my_shelf.close()
