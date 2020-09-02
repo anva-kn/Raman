d1b1491211349a8f4e428d07ee2dce163595b492
Anvar <kunanbaevanvar@gmail.com>
2020-09-02 20:52:12 +0800

    some more data preprocessing

--- keras_experiment_main.py
+++ keras_experiment_main.py
@@ -20,8 +20,32 @@ for key in my_shelf:
 my_shelf.close()
 
 one_spectrum = data_15000[0][0]
+print(data_15000.shape, f_sup.shape)
 
-for i in range(data_15000.shape[0][1]):
-    plt.plot(f_sup, data_15000[0][i], '-')#, label='one spectrum')
-#plt.legend()
+whole_data = np.zeros((10000, 1600))
+print(whole_data.shape)
+whole_data[0:1250] = np.copy(data_15000[0, 0:1250]/np.max(data_15000[0]))
+whole_data[1250:2500] = np.copy(data_15000[1, 0:1250]/np.max(data_15000[1]))
+whole_data[2500:3750] = np.copy(data_15000[2, 0:1250]/np.max(data_15000[2]))
+whole_data[3750:5000] = np.copy(data_15000[3, 0:1250]/np.max(data_15000[3]))
+whole_data[5000:6250] = np.copy(data_1500[0, 0:1250]/np.max(data_1500[0]))
+whole_data[6250:7500] = np.copy(data_1500[1, 0:1250]/np.max(data_1500[1]))
+whole_data[7500:8750] = np.copy(data_1500[2, 0:1250]/np.max(data_1500[2]))
+whole_data[8750:10000] = np.copy(data_1500[3, 0:1250]/np.max(data_1500[3]))
+labels = np.zeros(10000)
+labels[0:5000] = 0
+print(labels[5000])
+labels[5000:10000] = 1
+print(labels[5000])
+plt.plot(f_sup, whole_data[1299], '-')#, label='one spectrum')
+# plt.legend()
 plt.show()
+print(whole_data[9999])
+X_fn = './data/X_finetune.npy'
+y_fn = './data/y_finetune.npy'
+X = np.copy(whole_data)
+y = np.copy(labels)
+print(y)
+print(X.shape, y.shape)
+checksumbitch = np.where(whole_data == 0)
+indeces = np.unique(checksumbitch[0])
